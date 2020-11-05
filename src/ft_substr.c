6f1a241e92a044b52cfe28668cf33a66c4305354
SirMorfield <joppe.koers@gmail.com>
2020-11-05 15:47:38 +0100

    remove double definition

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -6,25 +6,13 @@
 /*   By: jkoers <jkoers@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/27 10:58:39 by jkoers        #+#    #+#                 */
-/*   Updated: 2020/11/01 19:23:42 by jkoers        ########   odam.nl         */
+/*   Updated: 2020/11/03 01:22:07 by jkoers        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 #include <stddef.h>
-
-static char	*ft_strndup(char *str, unsigned long len)
-{
-	char *dup;
-
-	dup = malloc((len + 1) * sizeof(char));
-	if (dup == NULL)
-		return (NULL);
-	ft_memcpy(dup, str, len * sizeof(char));
-	dup[len] = '\0';
-	return (dup);
-}
 
 char		*ft_substr(char const *s, unsigned int start, size_t len)
 {
