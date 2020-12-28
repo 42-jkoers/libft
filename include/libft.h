734aef9f4e08bc9e8e73dc5b8b16668e641f0877
SirMorfield <joppe.koers@gmail.com>
2020-12-28 20:38:33 +0100

    typo

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: jkoers <jkoers@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 14:51:21 by jkoers        #+#    #+#                 */
-/*   Updated: 2020/12/28 19:38:56 by jkoers        ########   odam.nl         */
+/*   Updated: 2020/12/28 20:38:24 by jkoers        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,7 +135,7 @@ void				ft_arr_voidp_shift(t_arr_voidp **arr, void (*del)(void *));
 void				ft_arr_voidp_pop(t_arr_voidp **arr, void (*del)(void *));
 void				**ft_arr_voidp_concat(t_arr_voidp *arr, size_t *length);
 double				ft_strtodbl(char *str);
-size_t				ft_strcousnt(char *str, char search);
+size_t				ft_strcount(char *str, char search);
 
 /*
 ** typedef struct		s_arr_long
