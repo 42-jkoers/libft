6b81247549b1848e3cb42207893266833725dad9
SirMorfield <joppe.koers@gmail.com>
2021-03-14 19:41:50 +0100

    add a lot

--- test/test_ft_buf.c
+++ test/test_ft_buf.c
@@ -1,42 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_split_file.c                                    :+:    :+:            */
+/*   test_ft_arr.c                                :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jkoers <jkoers@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/12/23 14:12:50 by jkoers        #+#    #+#                 */
-/*   Updated: 2020/12/26 13:13:03 by jkoers        ########   odam.nl         */
+/*   Created: 2020/12/26 12:18:35 by jkoers        #+#    #+#                 */
+/*   Updated: 2020/12/26 12:36:05 by jkoers        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
-char	**ft_split_file(const char *filename, size_t *num_lines)
+int	main(void)
 {
-	char		*read;
+	t_readbuf	buf;
 	int			fd;
-	t_arr		*arr;
+	char		*result;
 
-	fd = open((char *)filename, O_RDONLY);
+	fd = open(".gitignore", O_RDONLY);
 	if (fd < 0)
-		return (NULL);
-	arr = NULL;
-	while (ft_get_next_line(fd, &read) >= 0)
 	{
-		if (ft_arr_push(&arr, read) == NULL)
-		{
-			ft_arr_free(arr, free);
-			read = NULL;
-			*num_lines = 0;
-			close(fd);
-			return (NULL);
-		}
+		ft_putstr("Can't find file\n");
+		return (1);
 	}
-	close(fd);
-	return ((char **)ft_arr_concat(arr, num_lines));
+	ft_buf_init(&buf, 4096, fd);
+	while (ft_buf_read(&buf))
+	{
+	}
+	result = ft_buf_merge(&buf);
+	printf("%s", result);
+	free(result);
+	return (0);
 }
